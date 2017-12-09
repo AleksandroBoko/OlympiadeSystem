ddbcc4467a40efc5dc8b3102ee30f0c27bb5d1ce
Aleks B <aleksandroboko@gmail.com>
2017-12-09 18:19:05 +0200

    Fixed application start

--- OlympiadSystem/Program.cs
+++ OlympiadSystem/Program.cs
@@ -16,7 +16,7 @@ namespace OlympiadSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ());
+            Application.Run(new formPersonInfo());
         }
     }
 }
